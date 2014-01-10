aa274cd8196a805d40ac1ab20f3c638e0a605f60
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-10 02:29:08 +0000

    trying to add mathemtical operator generator

--- game.js
+++ game.js
@@ -5,23 +5,21 @@ var firstUserLives = 3;
 var secondUserLives = 3;
 var generatedNumber1 = Math.floor(Math.random() * 20 + 1);
 var generatedNumber2 = Math.floor(Math.random() * 20 + 1);
-var userAnswer = 
+var userAnswer = 0;
 
+console.log(generatedNumber1);
 
-if ( generatedNumber1= 0,0.33) {
-    console.log("+")
+if ((generatedNumber1) < 7) {
+    console.log("+");
 }
-else if (generatedNumber1 = 0.34,0.66 )
+else if (((generatedNumber1) >=7) && ((generatedNumber1) < 14)) {
+    console.log("*");
+}
+else (generatedNumber1 >= 14 && (generatedNumber1) < 21)
 {
-    console.log("*")
-}
-else (generatedNumber1 = 0.67,1 )
-{
-    console.log("-")
+    console.log("-"); 
 }
 
-<!-- Questions:
 
-How do I keep track of which user is answering what questions?
-How do I keep track of each user's remaining lives? -->
+
 
