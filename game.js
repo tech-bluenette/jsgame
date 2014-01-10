2a6a75a63c83f6fb432aa3e31101fbb02c3a6f48
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-10 01:57:33 +0000

    Added some text.

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-game
+
+var firstUser;
+var secondUser;
+var firstUserLives = 3;
+var secondUserLives = 3;
+var generatedNumber1 = Math.floor(Math.random() * 20 + 1);
+var generatedNumber2 = Math.floor(Math.random() * 20 + 1);
+var userAnswer = 
+
+
+if ( generatedNumber1= 0,0.33) {
+    console.log("+")
+}
+else if (generatedNumber1 = 0.34,0.66 )
+{
+    console.log("*")
+}
+else (generatedNumber1 = 0.67,1 )
+{
+    console.log("-")
+}
+
+<!-- Questions:
+
+How do I keep track of which user is answering what questions?
+How do I keep track of each user's remaining lives? -->
+
