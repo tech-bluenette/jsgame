cf929e121648216f7810d183a1d8b29bf45e57dd
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-10 03:34:14 +0000

    making progress

--- game.js
+++ game.js
@@ -1,29 +1,31 @@
 
-var firstUser;
-var secondUser;
 var firstUserLives = 3;
 var secondUserLives = 3;
 var generatedNumber1 = Math.floor(Math.random() * 20 + 1);
 var generatedNumber2 = Math.floor(Math.random() * 20 + 1);
-var userAnswer = 0;
+var userAnswer = prompt('What is the answer to:');
 
-console.log(generatedNumber1);
-
-if (generatedNumber1 < 7) {
+var operator = function () {
+  if (generatedNumber1 < 7) {
     console.log("+");
 } else if ((generatedNumber1 >=7) && (generatedNumber1 < 14)) {
     console.log("*");
 } else if ((generatedNumber1 >= 14) && (generatedNumber1 < 21)) {
     console.log("-"); 
 }
+}
 
-// if(generatedNumber1 < 7) {
-//   console.log("+");
-// } else if((generatedNumber1 >= 7) && (generatedNumber1 < 14)) {
-//   console.log("*");
-// } else ((generatedNumber1 >= 14) && (generatedNumber1 < 21)) {
-//   console.log("-");
-// }
+function question () {
+   console.log(generatedNumber1);
+   operator();
+   console.log(generatedNumber2);
+}
+
+
+console.log(question());    
+
+
+
 
 
 
